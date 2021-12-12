6877e9da7625593232731e20f265a28db4c41b13
João Vitor <71740709+Joao24820@users.noreply.github.com>
2021-12-12 10:18:28 -0300

    Create conta.py

---
+++ conta.py
@@ -0,0 +1,10 @@
+st = (gh / hm) * hm
+ipR = (11/100) * st
+inss = (8/100) * ipR
+sindi = (5/100) * inss
+sal = st - (sindi + inss + ipR)
+# Salario Bruto
+# Desconto referente ao imposto de renda de 11%
+# Desconto referente ao inss de 8%
+# Desconto referente ao Sindicado de 5%
+# Salario liquido
